cf688adf16d0ebf62d221ec330cd91be8a8adb47
Sona Nam <kihiall@gmail.com>
2023-08-21 10:33:37 +0900

    z-index

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-// 로그인,회원가입 모달창
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const signinButton = document.querySelector(".signinButton");
   const signupButton = document.querySelector(".signupButton");
-  const modal = document.querySelector(".modal"); // 모달 창을 나타내는 요소
+  const modal = document.querySelector(".modal");
 
-  signinButton.addEventListener("click", () => {
-    modal.style.display = "block";
+  signinButton.addEventListener("click", function () {
+    modal.style.display = "block"; // 모달 표시
   });
 
-  signupButton.addEventListener("click", () => {
-    modal.style.display = "block";
+  signupButton.addEventListener("click", function () {
+    modal.style.display = "block"; // 모달 표시
   });
 
-  window.addEventListener("click", (event) => {
+  // 모달 바깥을 클릭하면 모달 창 닫기
+  window.addEventListener("click", function (event) {
     if (event.target === modal) {
       modal.style.display = "none";
+    }
+  });
+});
+
+// 게시물 클릭시 z-index
+
+const boards = document.querySelectorAll(
+  ".post-list, .membershipboard, .fetimageboard, .hospitalboard, .groupbuying, .preuser, .missing"
+);
+const links = document.querySelectorAll("[data-target]");
+
+// 링크에 클릭 이벤트 리스너 추가
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    event.preventDefault();
+
+    // 클릭한 링크의 data-target
+    const targetBoard = this.getAttribute("data-target");
+
+    // 모든 게시판의 z-index 값을 0으로 초기화
+    boards.forEach((board) => {
+      board.style.zIndex = 0;
+    });
+
+    // 클릭한 링크에 게시판을 z-index 값을 2로설정
+    const targetElement = document.querySelector("." + targetBoard);
+    if (targetElement) {
+      targetElement.style.zIndex = 2;
     }
   });
 });
